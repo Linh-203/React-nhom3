7ebbb4b2a246b565b10590448a306622d9f1f56e
NamLê <115817757+namlvph28063@users.noreply.github.com>
2023-07-13 21:09:32 +0700

    Revert "add backend"

--- Backend/src/routers/auth.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { register } from "../controllers/auth";
-
-const router = express.Router()
-
-router.post("/auth/register", register)
-
-
-export default router
