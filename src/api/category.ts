3e08c7973361d93e83c7d8ab6cb4b2db510a2664
namle <namlvph28063@gmail.com>
2023-07-18 16:20:13 +0700

    update categories

--- src/api/category.ts
+++ src/api/category.ts
@@ -1,11 +1,9 @@
-import { AxiosResponse } from 'axios';
 import instance from './instance';
-import { ResponsePaginate } from '../common/product';
+
 import { ICategory } from '../common/category';
 
-
 export const createCate =async(data:ICategory)=>{
-  return await instance.post("categories",data)
+  return await instance.post("/categories",data)
 }
 export const getAllCategory = async (): Promise<ICategory[]> => {
   const res = await instance.get('/categories?_expand');
@@ -13,6 +11,10 @@ export const getAllCategory = async (): Promise<ICategory[]> => {
 };
  export const getCategoryById = async (id: string)=> {
   return await instance.get('/categories/' + id)
+
+}
+export const removeCategoryById = async (id: string)=> {
+  return await instance.delete('/categories/' + id)
 
 }
 export const updateCategory = async (id: string,data:ICategory) => {
--- src/pages/Admin/AddCategory.tsx
+++ src/pages/Admin/AddCategory.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {useState} from "react"
+import { useState } from "react"
 import { createCate } from "../../api/category";
 import { useNavigate } from "react-router-dom";
 const AddCategory = () => {
@@ -8,59 +8,64 @@ const AddCategory = () => {
   const [cateImg, setCateImg] = useState()
   const [cateImgErr, setCateImgErr] = useState(false)
   const navigate = useNavigate()
-  const onChangeName = (e:any) => {
-  const value = e.target.value
-  setCateName(value)
-  onBlurName(value)
+  const onChangeName = (e: any) => {
+    const value = e.target.value
+    setCateName(value)
+    onBlurName(value)
   }
-  const onBlurName =(value:string)=>{
-    if(value.length<=0){
-    setCateNameErr(true) 
-    }else{
-      setCateNameErr(false) 
+  const onBlurName = (value: string) => {
+    if (value.length <= 0) {
+      setCateNameErr(true)
+    } else {
+      setCateNameErr(false)
     }
   }
-  
+
 
   console.log(cateNameErr);
-  
-  const onSubmit =async (e:any)=>{
+
+  const onSubmit = async (e: any) => {
     e.preventDefault()
+
+    if (cateName.length <= 0) {
+      setCateNameErr(true)
+      return
+    }
     console.log(e.target[1].files);
-  const fileList = e.target[1].files
+    const fileList = e.target[1].files
     const formData = new FormData()
-    for(const file of fileList) {
+    for (const file of fileList) {
       formData.append('images', file)
     }
-  
-  await  axios.post('http://localhost:8000/api/upload', formData)
+
+    await axios.post('http://localhost:8000/api/upload', formData)
       .then((response) => {
         console.log(response.data.data[0].url);
-       setCateImg(response.data.data[0].url);
-        
+        setCateImg(response.data.data[0].url);
+
       })
       .catch((error) => {
         console.log(error);
       });
 
-      if(cateImg && cateName){
-        const data={
-          name:cateName,
-          image:cateImg
-        }
-        console.log(data);
-        
-        createCate(data)
-         alert("oki")
-         navigate("/admin/categories")
+    if (cateImg && cateName.length > 0) {
+      const data = {
+        name: cateName,
+        image: cateImg
       }
-  }
-  
+      console.log(data);
 
-  
+      createCate(data)
+      alert("oki")
+      navigate("/admin/categories")
+    }
+  }
+
+
+
   return (
     <div>
-       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             className="mx-auto h-10 w-auto"
@@ -68,7 +73,7 @@ const AddCategory = () => {
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          AddCategory
+            AddCategory
           </h2>
         </div>
 
@@ -83,13 +88,13 @@ const AddCategory = () => {
                   id="email"
                   name="name"
                   type="text"
-                onChange={onChangeName}
-                onBlur={()=>onBlurName(cateName)}
-                  required
+                  onChange={onChangeName}
+                  onBlur={() => onBlurName(cateName)}
+
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-              <p style={{color:"#f12"}}>{cateNameErr ? "This is required" :""}</p>
+              <p style={{ color: "#f12" }}>{cateNameErr ? "This is required" : ""}</p>
             </div>
 
             <div>
@@ -97,15 +102,15 @@ const AddCategory = () => {
                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                   Image
                 </label>
-               
+
               </div>
               <div className="mt-2">
                 <input
-           
+
                   id="password"
                   name="image"
                   type="file"
-                  required
+
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -116,12 +121,12 @@ const AddCategory = () => {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-               Submit
+                Submit
               </button>
             </div>
           </form>
 
-         
+
         </div>
       </div>
     </div>
