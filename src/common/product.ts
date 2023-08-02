ec66d742ec1bb9ae3a7bd45f972c71535a95d318
screaminGreen <101007489+hiepfre3101@users.noreply.github.com>
2023-08-02 20:53:35 +0700

    variations #1

--- src/common/product.ts
+++ src/common/product.ts
@@ -1,5 +1,5 @@
 // dung de data:any o day xin day !!!!!
-export type IProduct = {
+export interface IProduct  {
    _id: string;
    name: string;
    favorite: number;
@@ -8,17 +8,19 @@ export type IProduct = {
    categoryId: string;
    stock: number;
    solded: number;
+   variations: IVariation[];
    images: { url: string; public_id?: string; _id?: string }[];
    discount: number;
-};
+}
+
 export type InputProduct = {
    name: string;
    price: number;
    desc: string;
    categoryId: string;
-   stock: number;
    images: string | { url: string; public_id?: string; _id?: string }[];
    discount: number;
+   variations: IVariation[];
 };
 export type ResponsePaginate<T> = {
    message: string;
@@ -32,4 +34,17 @@ export type IPaginate = {
    currentPage: number;
    totalPages: number;
    totalItems: number;
+};
+
+export type IVariation = {
+   _id: string;
+   weight: number;
+   quantity: number;
+   vendorId: string;
+};
+
+export type IVendor = {
+   _id: string;
+   name: string;
+   origin: string;
 };
